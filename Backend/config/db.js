53403c392eda5f1788e294601940dc069011ba43
Prashant Raj <prashantraj01122@gmail.com>
2025-06-20 21:48:16 +0530

    .

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -4,11 +4,3 @@ export const connectDB = async()=>{
     await mongoose.connect('mongodb+srv://prashantraj01122:5BH3LxpKP4tnVcXn@food-delivery-db.41tdlvk.mongodb.net/Food-Delivery').then(()=>console.log("DB Connected"));
 }
 
-
-
-//password - 5BH3LxpKP4tnVcXn    prashantraj01122
-
-
-//    
-
-//mongodb+srv://prashantraj01122:5BH3LxpKP4tnVcXn@food-delivery-db.41tdlvk.mongodb.net/
