091ff8c6bb4f0ef7834dbd02aaa33a5e75576d88
Prashant Raj <prashantraj01122@gmail.com>
2025-07-02 14:02:47 +0530

    axios solve problem

--- Frontend/src/pages/MyOrders/MyOrders.jsx
+++ Frontend/src/pages/MyOrders/MyOrders.jsx
@@ -3,7 +3,6 @@ import './MyOrders.css'
 import { StoreContext } from '../../context/StoreContext';
 import axios from 'axios';
 import { assets } from '../../assets/assets';
-import axiosInstance from '../../axiosInstance';
 
 const MyOrders = () => {
 
@@ -11,7 +10,7 @@ const MyOrders = () => {
     const [data, setData] = useState([]);
 
     const fetchOrders = async () => {
-        const response = await axiosInstance.post(url + "/api/order/userorders",{} , {headers:{token}});
+        const response = await axios.post(url + "/api/order/userorders",{} , {headers:{token}});
         setData(response.data.data);
     }
 
--- Frontend/src/pages/Verify/Verify.jsx
+++ Frontend/src/pages/Verify/Verify.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect } from 'react'
 import "./Verify.css"
 import { useNavigate, useSearchParams } from 'react-router-dom'
 import { StoreContext } from '../../context/StoreContext';
-import axiosInstance from '../../axiosInstance';
+import axios from 'axios';
 
 const Verify = () => {
 
@@ -14,7 +14,7 @@ const Verify = () => {
     
 
     const verifyPayment = async () => {
-        const response = await axiosInstance.post(url + "/api/order/verify" , {success,orderId});
+        const response = await axios.post(url + "/api/order/verify" , {success,orderId});
         if(response.data.success){
             navigate("/myorders")
         }
